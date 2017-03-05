bae6781f7e9e5798fac7cdec99f4cd0f7d1535ba
Hebron George <hebrontgeorge@gmail.com>
2017-03-04 23:48:25 -0500

    reading in server config file

--- nexy.py
+++ nexy.py
@@ -10,18 +10,27 @@
 
 import irc.bot
 import logging
+from ConfigParser import SafeConfigParser
 
 class NexyBot(irc.bot.SingleServerIRCBot):
 	pass
 
 def main():
-	logger = logging.getLogger()
+	setupLogger()
+	logger = logging.getLogger('nexy')
+
+	conf = SafeConfigParser()
+	conf.read('config/servers.ini')
+
+	logger.debug('Connecting to servers: {}'.format(conf.sections()))
+
+def setupLogger():
+	logger = logging.getLogger('nexy')
 	handler = logging.StreamHandler()
 	formatter = logging.Formatter('%(asctime)s %(name)-12s %(levelname)-8s %(message)s')
 	handler.setFormatter(formatter)
 	logger.addHandler(handler)
 	logger.setLevel(logging.DEBUG)
-	logger.debug("Hello World!")
 
 if __name__ == "__main__":
 	main()
