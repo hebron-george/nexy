1317824228fd3a4b377baf760a070e13c6308750
Hebron George <hebrontgeorge@gmail.com>
2018-03-31 22:39:55 -0500

    make it work

--- lib/nexy.rb
+++ lib/nexy.rb
@@ -1,38 +1,14 @@
 require 'thor'
+require 'io/console'
 require 'nexy/version'
 
 module Nexy
-  class ThorShell < Thor
+  class ThorShell < Thor::Shell::Color
 
     def ask_securely(*args)
       # TODO: Ask args and securely get response
       STDIN.noecho(&:gets).chomp
     end
-    desc 'setup', 'Walk through nexy configuration setup'
-    def setup
-      # config = get_current_config
-      # update_config! config
-    end
-
-    desc "create-ruby-app <name>", "create a new plain old Ruby project"
-    def create_ruby_app(name)
-      type = :ruby.freeze
-      config = create_project_config(name: name, type: type)
-    end
-
-    desc "create-rails-app <name>", "create a new Ruby on Rails application"
-    def create_rails_app(name)
-      type = :rails.freeze
-      config = create_project_config(name: name, type: type)
-    end
-
-    desc "create-python-app <name>", "create a new plain old Python project"
-    def create_python_app(name)
-      type = :python.freeze
-      config = create_project_config(name: name, type: type)
-    end
-
-    private
 
     def create_project!(project_configuration)
 
--- lib/nexy/cli.rb
+++ lib/nexy/cli.rb
@@ -1,8 +1,32 @@
-require 'nexy'
 require 'thor'
+require 'nexy'
 
 module Nexy
   class CLI < Thor
+    include Thor::Actions
 
+    desc 'setup', 'Walk through nexy configuration setup'
+    def setup
+      # config = get_current_config
+      # update_config! config
+    end
+
+    desc "create-ruby-app <name>", "create a new plain old Ruby project"
+    def create_ruby_app(name)
+      type = :ruby.freeze
+      config = create_project_config(name: name, type: type)
+    end
+
+    desc "create-rails-app <name>", "create a new Ruby on Rails application"
+    def create_rails_app(name)
+      type = :rails.freeze
+      config = create_project_config(name: name, type: type)
+    end
+
+    desc "create-python-app <name>", "create a new plain old Python project"
+    def create_python_app(name)
+      type = :python.freeze
+      config = create_project_config(name: name, type: type)
+    end
   end
 end
