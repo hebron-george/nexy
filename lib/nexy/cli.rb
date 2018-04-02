7c6f15aadde8ef86d9b07a069e653c27bb096fe0
Hebron George <hebrontgeorge@gmail.com>
2018-04-01 23:52:03 -0500

    I like single quotes more

--- lib/nexy/cli.rb
+++ lib/nexy/cli.rb
@@ -11,19 +11,19 @@ module Nexy
       # update_config! config
     end
 
-    desc "create-ruby-app <name>", "create a new plain old Ruby project"
+    desc 'create-ruby-app <name>', 'create a new plain old Ruby project'
     def create_ruby_app(name)
       type = :ruby.freeze
       config = create_project_config(name: name, type: type)
     end
 
-    desc "create-rails-app <name>", "create a new Ruby on Rails application"
+    desc 'create-rails-app <name>', 'create a new Ruby on Rails application'
     def create_rails_app(name)
       type = :rails.freeze
       config = create_project_config(name: name, type: type)
     end
 
-    desc "create-python-app <name>", "create a new plain old Python project"
+    desc 'create-python-app <name>', 'create a new plain old Python project'
     def create_python_app(name)
       type = :python.freeze
       config = create_project_config(name: name, type: type)
