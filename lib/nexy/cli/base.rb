fb2499ebdf4ede68eec347419089a453a0551adf
Hebron George <hebrontgeorge@gmail.com>
2018-04-02 00:37:42 -0500

    base cli

--- lib/nexy/cli/base.rb
+++ lib/nexy/cli/base.rb
@@ -1,7 +1,22 @@
-require 'avant/cli'
+require 'nexy/cli'
 
 module Nexy
   class CLI::Base
+    def self.run(options)
+      new(options).run
+    end
 
+    attr_reader :options
+    def initialize(options)
+      @options = options.dup
+    end
+
+    def ui
+      ::Nexy.ui
+    end
+
+    def run
+      raise NotImplementedError, 'Must be implemented by a subclass'
+    end
   end
 end
