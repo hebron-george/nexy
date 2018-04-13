08ce6291347840d2f134877532cf4cf79bfc6c1f
Hebron George <hebrontgeorge@gmail.com>
2018-04-13 09:45:55 -0500

    wip

--- lib/nexy/cli/setup.rb
+++ lib/nexy/cli/setup.rb
@@ -2,10 +2,22 @@ require 'nexy/cli/base'
 
 module Nexy
   class CLI::Setup < CLI::Base
-    CONFIG_FILE = '~/.nexy'.freeze
-
     def run
-      ui.say('Beginning setup...')
+      if Nexy::Config.config_file_exists?
+        update_existing_config!
+      else
+        create_new_config!
+      end
+    end
+
+    private
+
+    def update_existing_config!
+      ui.say("An existing config file was found at: #{Nexy::Config::CONFIG_PATH}")
+    end
+
+    def create_new_config!
+      ui.say("Let's set up nexy to work for you!")
     end
   end
 end
--- lib/nexy/config.rb
+++ lib/nexy/config.rb
@@ -8,8 +8,54 @@ module Nexy
     CONFIG_PATH      = '~/.nexy'
     FULL_CONFIG_PATH = File.expand_path(CONFIG_PATH)
 
+    FIELDS = %w{ projects_path }
+
+    def config_object
+      if config_file_exists?
+        @config_object ||= read_config_file
+      else
+        @config_object ||= Hash.new
+      end
+    end
+
     def config_file_exists?
       File.file?(FULL_CONFIG_PATH)
+    end
+
+    def write_config_file
+      File.open(FULL_CONFIG_PATH, 'w') { |f| f.write config_object.to_yaml }
+    end
+
+    # This will build all of the accessors for generic fields. If you need custom
+    # functionality, just define the accessor method above this code and name it
+    # appropriately.
+    # my_field_exists?
+    # my_field    -reader
+    # my_field=   -writer
+    (FIELDS).each do |field|
+      unless self.method_defined?("#{field}_exists?")
+        define_method("#{field}_exists?") do
+          config_file_exists? && config_object.has_key?(field)
+        end
+      end
+
+      unless self.method_defined?(field)
+        define_method(field) do
+          config_object.fetch(field, nil)
+        end
+      end
+
+      unless self.method_defined?("#{field}=")
+        define_method("#{field}=") do |value|
+          config_object[field] = value
+        end
+      end
+    end
+
+    private
+
+    def read_config_file
+      YAML.load_file(FULL_CONFIG_PATH)
     end
   end
 end
