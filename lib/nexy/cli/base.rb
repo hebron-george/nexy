a7f84cf4e2b6323869f026f89d1d2c1bbf5bc047
Hebron George <hebrontgeorge@gmail.com>
2018-04-28 21:41:10 -0500

    add an optional required_keys check

--- lib/nexy/cli/base.rb
+++ lib/nexy/cli/base.rb
@@ -1,9 +1,24 @@
 require 'nexy/cli'
+require 'nexy/config'
 
 module Nexy
   class CLI::Base
     def self.run(options)
+      abort "Your setup is incomplete, try: `nexy setup`" unless required_keys.all? { |key| Nexy::Config.config_object.keys.include? key }
       new(options).run
+    end
+
+    # The required keys needed in our
+    # config file (Nexy::Config.config_object)
+    # in order for this class (or it's descendants)
+    # to properly function.
+    #
+    # E.g. the `create` function creates new projects,
+    # therefore it requires that the `projects_path`
+    # be configured, otherwise it doesn't know where to
+    # create the project.
+    def self.required_keys
+      []
     end
 
     attr_reader :options
